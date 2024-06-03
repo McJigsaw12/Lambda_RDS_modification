65d246e6673eb0fa50564b2c0592e582e9f92fe9
McJigsaw12 <39839392+McJigsaw12@users.noreply.github.com>
2024-06-03 11:08:40 -0700

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -4,7 +4,7 @@ import botocore
 import boto3
 
 def lambda_handler(event, context):
-    # TODO implement
+    # TODO implement https://dev.to/aws-builders/modify-the-aws-rds-instance-size-using-lambda-function-1l71
     boto3.client('rds').modify_db_instance(DBInstanceIdentifier='nombre-rds', 
     DBInstanceClass='db.m5.2large',ApplyImmediately=True) 
     
